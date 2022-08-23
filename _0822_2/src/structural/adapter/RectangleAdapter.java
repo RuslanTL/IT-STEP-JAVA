0f80bdcb3b08e39b021d8975b815dc2182da6331
plaz <53999108+RuslanTL@users.noreply.github.com>
2022-08-23 16:36:38 +0600

    0823 2

--- _0822_2/src/structural/adapter/RectangleAdapter.java
+++ _0822_2/src/structural/adapter/RectangleAdapter.java
@@ -6,7 +6,11 @@ public class RectangleAdapter implements Shape{
         this.adaptee = adaptee;
     }
     @Override
-    public void draw(int x, int y, int z, int j) {
-        adaptee.draw(x,y,z,j);
+    public void draw(int x, int y, int z, int w) {
+        int x1 = Math.min(x,y);
+        int y1 = Math.min(z,w);
+        int width = Math.abs(z-x);
+        int height = Math.abs(w-y);
+        adaptee.draw(x1,y1,width,height);
     }
 }
